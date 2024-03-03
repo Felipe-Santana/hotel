a196fcef0d1b52c97868c2f73bd04cf68baf47be
Felipe Santana <felipesantanaalbuquerque9@gmail.com>
2024-03-03 09:51:54 -0300

    🔥 Remove id from images route path

--- src/room/routes/room.ts
+++ src/room/routes/room.ts
@@ -13,11 +13,11 @@ export class RoomRouter {
   }
 
   load(router: Router, fileRepository: FileRepository) {
-    this._.post('/', this.roomController.create.bind(this.roomController));
+    this._.post('/', this.roomController.create());
     this._.get('/', this.roomController.list.bind(this.roomController));
     this._.delete('/:id', this.roomController.delete.bind(this.roomController));
     this._.get('/:id', this.roomController.get.bind(this.roomController));
-    this._.post('/:id/images', uploadImagesMiddleware(fileRepository, 'rooms'));
+    this._.post('/images', uploadImagesMiddleware(fileRepository, 'rooms'));
     router.use('/rooms', this._);
   }
 }
