bd4058ed4f2c38d8500f074975a442de682d476c
Felipe Santana <felipesantanaalbuquerque9@gmail.com>
2024-03-03 09:53:23 -0300

    ♻️ Return handler function from controller

--- src/room/controllers/roomController.ts
+++ src/room/controllers/roomController.ts
@@ -18,21 +18,23 @@ export class RoomController {
     this.roomRepository = roomRepository;
   }
 
-  async create(req: Request, res: Response) {
-    try {
-      if (!req.body.room) return res.status(400).json({ code_err: ErrorCode.MISSING_ROOM, message: 'Missing room in body' });
+  create() {
+    return async (req: Request, res: Response) => {
+      try {
+        if (!req.body.room) return res.status(400).json({ code_err: ErrorCode.MISSING_ROOM, message: 'Missing room in body' });
 
-      const room = RoomActions.createRoom(req.body.room);
+        const room = RoomActions.createRoom(req.body.room);
 
-      const { insertedId } = await this.roomRepository.saveRoom(room);
+        const { insertedId } = await this.roomRepository.saveRoom(room);
 
-      return res.status(201).json({ id: insertedId });
-    } catch (error) {
-      return res.status(500).json({ code_err: ErrorCode.CREATE_ERROR, message: error.message });
+        return res.status(201).json({ id: insertedId });
+      } catch (error) {
+        return res.status(500).json({ code_err: ErrorCode.CREATE_ERROR, message: error.message });
+      }
     }
   }
 
-  async list(req: Request, res: Response) {
+  async list(_: Request, res: Response) {
     try {
       const rooms = await this.roomRepository.list();
 
